c66c672267c31d798f5224abd35a84a9b41d6df7
Victor || VickyJay <77025670+Victorola-coder@users.noreply.github.com>
2025-09-20 12:20:51 +0100

    refactor: adjust footer layout and styling for improved alignment and responsiveness

--- app/components/atoms/footer.tsx
+++ app/components/atoms/footer.tsx
@@ -2,11 +2,11 @@ import { InstagramIcon, TwitterIcon } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="bg-[#FB41DC] m-[20px] md:m-[48px] rounded-[16px] md:rounded-[24px] py-[40px] md:py-[95px] px-[20px] md:px-[73px] flex flex-col md:flex-row items-center md:items-center gap-[40px] md:gap-[220px]">
-      <h2 className="font-franklin font-semibold text-[40px] md:text-[128px] leading-[90%] md:leading-[100%] tracking-[-2%] md:tracking-[-3%] text-white text-center md:text-left">
+    <footer className="bg-[#FB41DC] m-[20px] md:m-[48px] rounded-[16px] md:rounded-[24px] py-[40px] md:py-[95px] px-[20px] md:px-[73px] flex flex-col md:flex-row items-start md:items-center gap-[40px] md:gap-[220px]">
+      <h2 className="font-franklin font-semibold max-w-[173px] md:max-w-min text-[40px] md:text-[128px] leading-[90%] md:leading-[100%] tracking-[-2%] md:tracking-[-3%] text-white text-left">
         Connect with us today.
       </h2>
-      <div className="flex flex-col gap-[20px] md:gap-[103px] items-center md:items-start">
+      <div className="flex flex-col gap-[20px] md:gap-[103px] items-start">
         <div className="font-franklin font-normal text-[14px] md:text-[16px] leading-[120%] tracking-[-2%] text-[#FFFFFF] flex flex-col gap-2">
           <figure className="flex flex-row items-center gap-[13px]">
             <img
